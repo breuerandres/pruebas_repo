60f39fccd1ebf4d08ebbae786a35ac49ad6ebaa8
Andres Breuer <breuer.andres@gmail.com>
2025-03-09 17:38:21 -0300

    nuevas tablas

--- src/routes/message_router.py
+++ src/routes/message_router.py
@@ -62,11 +62,7 @@ async def response(req: Request):
         "to": form_data['From'],
     }
 
-    listid_data = form_data["ListId"]
-
-    print(listid_data)
-
-    if not listid_data:
+    if not "ListId" in form_data:
         twilio_params["content_sid"] = "HX36a645432d650430b76ac3d77b0daa27"
         TwilioClient().send_message(**twilio_params)
         return "OK con error de input"
