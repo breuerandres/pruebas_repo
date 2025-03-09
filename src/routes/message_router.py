bc5058fa8c931f2ed6a64e25b6e554b9d2b5c57f
Andres Breuer <breuer.andres@gmail.com>
2025-03-09 17:19:15 -0300

    nuevas tablas

--- src/routes/message_router.py
+++ src/routes/message_router.py
@@ -55,18 +55,17 @@ async def response(req: Request):
     for key, value in form_data.items():
         print(f"{key}: {value}")
 
-    body = form_data["Body"]
     nro_pregunta, puntaje = form_data["ListId"].split("-")
     print(form_data['From'])
-    rtas_correctas = ['1', '2', '3', '4', '5']
+
     twilio_params = {
         "to": form_data['From'],
     }
 
-    # if (puntaje not in rtas_correctas):
-    #     twilio_params["content_sid"] = "HX36a645432d650430b76ac3d77b0daa27"
-    #     TwilioClient().send_message(**twilio_params)
-    #     return "OK con error de input"
+    if (not form_data.get("ListId")):
+        twilio_params["content_sid"] = "HX36a645432d650430b76ac3d77b0daa27"
+        TwilioClient().send_message(**twilio_params)
+        return "OK con error de input"
 
     if (nro_pregunta == "pregunta_1"):
         twilio_params["content_sid"] = "HX804140b99b23eeb9b26cdc5c27dc1d23"
