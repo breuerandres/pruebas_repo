bac42da1778f60ae86c2ce23dd5d034f13f72bd7
Andres Breuer <breuer.andres@gmail.com>
2025-03-09 18:08:37 -0300

    nuevas tablas

--- src/routes/message_router.py
+++ src/routes/message_router.py
@@ -51,17 +51,21 @@ async def response(req: Request):
     print("llega algo")
     print("Datos recibidos en el webhook:")
     form_data = await req.form()
-    print("Tipo de form_data:", type(form_data))
 
     for key, value in form_data.items():
         print(f"{key}: {value}")
 
-    print("Claves en form_data:", list(form_data.keys()))
+    if "ListId" not in form_data.keys():
 
-    if "ListId" not in form_data:
         print("No habia listid")
         twilio_params["content_sid"] = "HX36a645432d650430b76ac3d77b0daa27"
-        TwilioClient().send_message(**twilio_params)
+        print("ok con param")
+        try:
+            TwilioClient().send_message(**twilio_params)
+
+        except Exception as e:
+            print(e)
+
         return "OK con error de input"
 
     nro_pregunta, puntaje = form_data["ListId"].split("-")
