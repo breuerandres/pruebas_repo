6edbc177b84b14ab19bc9d9a008a095f7492df83
Andres Breuer <breuer.andres@gmail.com>
2025-03-11 00:29:25 -0300

    probando cookies

--- src/routes/message_router.py
+++ src/routes/message_router.py
@@ -76,8 +76,7 @@ def send_survey(user: auth_deps, db: db_deps, body: SendSurvey, response: Respon
     TwilioClient().send_message(**twilio_params_1)
     TwilioClient().send_message(**twilio_params_2)
 
-    response = JSONResponse(content="Message sent successfully.", headers={
-                            'set-cookie': f"{body.telefono}={json.dumps(cookie_dic)}"})
+    response = JSONResponse(content="Message sent successfully.")
 
     response.set_cookie(key=f"{body.telefono}",
                         value=json.dumps(cookie_dic))
@@ -86,7 +85,7 @@ def send_survey(user: auth_deps, db: db_deps, body: SendSurvey, response: Respon
 
 
 @message_router.post("/response", status_code=status.HTTP_200_OK)
-async def response(db: db_deps, req: Request, tel: str = Cookie(None)):
+async def response(db: db_deps, req: Request, tel: str = Cookie()):
     # Extraigo data del cuerpo del request
     print(tel)
     form_data = await req.form()
