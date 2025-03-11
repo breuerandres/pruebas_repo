88e8fe16ab5d061c9f0ccae15b9b2b1b0d8b8a00
Andres Breuer <breuer.andres@gmail.com>
2025-03-11 00:12:28 -0300

    probando cookies

--- src/routes/message_router.py
+++ src/routes/message_router.py
@@ -92,7 +92,7 @@ async def response(db: db_deps, req: Request):
 
     # Traigo info de cookies del tel "x"
     cookie_name = f"+{form_data['WaId']}"
-    print("2")
+    print(cookie_name)
     cookie_value = req.cookies.get(cookie_name)
     print("3")
     print(cookie_value)
